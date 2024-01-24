31c2757e07b0b458c20ed33794cc1e0b942f3c62
Denilson T. Souza <67978519+DhenSouza@users.noreply.github.com>
2024-01-24 18:17:23 -0300

    feat: Exception classes for the Card and Game domains created

---
+++ src/main/java/com/dhentech/catalogCardGames/domain/exceptions/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.dhentech.catalogCardGames.domain.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends BusinessException {
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(String msg) {
+        super(msg);
+    }
+}
