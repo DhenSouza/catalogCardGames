4bad9bc6e0231607e7c4b03adac6db82f637a5b3
Denilson T. Souza <67978519+DhenSouza@users.noreply.github.com>
2024-01-24 10:45:57 -0300

    feat: Att name past

---
+++ src/main/java/com/dhentech/catalogCardGames/CatalogCardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.dhentech.catalogCardGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogCardGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogCardGamesApplication.class, args);
+	}
+
+}
